891956c4fb67b280a3d1e1118a63b41392dcfae4
anirudh363 <145224957+anirudh363@users.noreply.github.com>
2025-03-12 00:51:55 +0530

    Create API route for Changing Password

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -21,7 +21,43 @@ def login():
         return jsonify({"message": result["error"]}), 401  # Unauthorized
     return jsonify({"message": "Login successful", "data": result}), 200  # OK
 
+@auth_bp.route("/change-password", methods=["POST"])
+def change_password():
+    data = request.get_json()
+    result = AuthController.change_password(data["email"], data["old_password"], data["new_password"])
+    
+    if "error" in result:
+        return jsonify({"message": result["error"]}), 404
+    return jsonify({"message": "Password changed successfully", "data": result}), 200
+
+
 @auth_bp.route("/users", methods=["GET"])
 def get_users():
     users = AuthController.get_users()
     return jsonify({"message": "Successfully retrieved users", "users": users}), 200
+
+@auth_bp.route("/users/<int:user_id>", methods=["GET"])
+def get_user(user_id):
+    user = AuthController.get_user_by_id(user_id)
+    if user:
+        return jsonify({"message": "Successfully retrieved user", "user": user}), 200
+    return jsonify({"message": "User not found"}), 404
+
+@auth_bp.route("/users/<int:user_id>", methods=["PUT"])
+def update_user(user_id):
+    data = request.get_json()
+    user = AuthController.update_user(user_id, data)
+    if user:
+        return jsonify({"message": "User updated successfully", "user": user}), 200
+    return jsonify({"message": "User not found"}), 404
+
+@auth_bp.route("/users/<int:user_id>", methods=["DELETE"])
+def delete_user(user_id):
+    user = AuthController.get_user_by_id(user_id)
+    if not user:
+        return jsonify({"message": "User not found"}), 404
+
+    status = AuthController.delete_user(user_id)
+    if status:
+        return jsonify({"message": "User deleted successfully"}), 200
+    return jsonify({"message": "User not found"}), 404
